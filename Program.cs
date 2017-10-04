bf7d24ed91f62a7f87bc52bfb0b1262806796e61
mattyDunn <31864601+mattyDunn@users.noreply.github.com>
2017-10-04 13:28:34 -0600

    End Of Fourth Video

--- Program.cs
+++ Program.cs
@@ -6,8 +6,10 @@ namespace HelloWorld
     {
         public static void Main(string[] args)
         {
-            Console.WriteLine("Hello World!");
-            string hello = Console.ReadLine();
-        }
+			Console.WriteLine("Write Two Numbers");
+            float hello1 = float.Parse(Console.ReadLine());
+            float hello2 = float.Parse(Console.ReadLine());
+			Console.WriteLine(hello1 + hello2);
+		}
     }
 }

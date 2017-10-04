d6f7d51b9619b9ba46015221100759791bf20996
mattyDunn <31864601+mattyDunn@users.noreply.github.com>
2017-10-04 13:17:43 -0600

    update

--- Program.cs
+++ Program.cs
@@ -7,7 +7,7 @@ namespace HelloWorld
         public static void Main(string[] args)
         {
             Console.WriteLine("Hello World!");
-            Console.WriteLine("Hello");
+            string hello = Console.ReadLine();
         }
     }
 }
